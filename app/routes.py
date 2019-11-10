0c17c3b4ea404b5a381e717bb7e8ca431f986bd6
Christina Lukaszczyk <clukas93@gmail.com>
2019-11-09 21:45:51 -0500

    made it a little prettier, remember to install flask-bootstrap

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,20 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
+class User:
+    def __init__(self,id,username,fname,lname):
+        self.id = id
+        self.username = username
+        self.fname = fname
+        self.lname = lname
+
+
 # stand-in user for testing
-user = {'fname': 'Eleni', 'lname': 'Lukaszczyk'}
+this_user = User(101, 'elukas', 'Eleni', 'Lukaszczyk')
+
+# list of stand-in other users
+userList = [User(101, 'elukas', 'Eleni', 'Lukaszczyk'),User(102, 'a_opoulos', 'Angela', 'Christopoulos'),User(103, 'groth', 'Grant', 'Rothenbecker')]
+
 
 # decorators modify the function that follows them
 # home page
@@ -12,16 +24,16 @@ user = {'fname': 'Eleni', 'lname': 'Lukaszczyk'}
 def index():
     posts = [
         {
-            'author': {'username': 'John'},
+            'author': userList[2],
             'body': 'I have the notes for chapter 7'
         },
         {
-            'author': {'username': 'Susan'},
+            'author': userList[1],
             'body': 'Is NaCl a strong or weak electrolyte?'
         }
     ]
 
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', user=this_user, posts=posts)
 
 # login page
 @app.route('/login', methods=['GET', 'POST'])
@@ -30,7 +42,7 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect(url_for('/index'))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Log In', form=form)
 
 # notes page
@@ -65,5 +77,13 @@ def groups():
 # profile page
 @app.route('/profile')
 def profile():
-    return render_template('profile.html', title='Profile', user=user)
+    return render_template('profile.html', title='Profile', user=this_user)
+
+@app.route('/user/<username>')
+def user(username):
+    for user in userList:
+        if user.username == username:
+            return render_template('user.html', user=user)
+
+    return render_template('404.html')
 
